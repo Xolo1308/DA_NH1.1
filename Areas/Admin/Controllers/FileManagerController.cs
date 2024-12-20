5fd51a8e117965887451604657b73de9e0da21b4
thaianh-R <nguyenthaianh270397@gmail.com>
2024-12-20 20:22:17 +0700

    20/12 quản lý ảnh

---
+++ Areas/Admin/Controllers/FileManagerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DA_NH.Areas.Admin.Controllers
+{
+    public class FileManagerController : Controller
+    {
+        [Area("Admin")]
+        [Route("/Admin/file-manager")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
