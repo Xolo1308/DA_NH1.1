91e0434d8973432f9ef7a728264e0fb6b586aa13
Nguyen khanh toan <nguyenkhanhtoan588@gmail.com>
2024-12-25 10:06:27 +0700

    25/12 làm về thêm giỏ h

---
+++ Models/CartItemModel.cs
@@ -0,0 +1,34 @@
+﻿namespace DA_NH.Models
+{
+    public class CartItemModel
+    {
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public int Quantity {  get; set; }
+
+        public int Price { get; set; }
+        public string? Image { get; set; }
+        public int Total {  
+            get {  return Quantity*Price; }
+         }
+        public CartItemModel()
+        {   
+
+        }
+        public CartItemModel(MenuItem menuitem)
+        {
+            ProductId = menuitem.MenuItemId;
+            ProductName = menuitem.Name;
+            Price = menuitem.Price;
+            Quantity = 1;
+            Image = menuitem.ImageUrl;
+
+
+        }
+        
+
+       
+
+     
+    }
+}
